cd059adb5712c932e51d71f9a265671de1eb0ae6
Vane-Arellano <vanessa.arellano@encora.com>
2025-01-11 13:44:15 -0600

    Update ShortestPalindrome.java

--- Java/ShortestPalindrome.java
+++ Java/ShortestPalindrome.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int days = prices.length;
-        int[] left = new int[days]; int[] right = new int[days]; 
-        int left_min = prices[0];
-        int right_max = prices[days-1];
-        int profit = right[0];
-
-        if(days == 1){
-            return profit;
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
         }
 
-        left[0] = 0; 
-        right[days-1] = 0;
+        String rev_s = new StringBuilder(s).reverse().toString();
+        String new_s = s + "*" + rev_s;
 
-        for(int i=1; i<days; i++){
-            left[i] = Math.max(left[i-1], prices[i] - left_min); 
-            left_min = Math.min(left_min, prices[i]);
-        }
-        for(int i = days-2; i >= 0; i--){
-            right[i] = Math.max(right[i+1], right_max - prices[i]); 
-            right_max = Math.max(right_max, prices[i]);
-        }
-        
-        profit = right[0];
-        for(int i = 1; i < days-1; i++){
-            profit = Math.max(profit, left[i-1]+right[i]);
-        }
+        int[] lps = new int[new_s.length()];
 
-        return profit;
-    
-    
+        for (int i = 1; i < new_s.length(); i++) {
+            
+            int j = lps[i - 1];
+            while (j > 0 && new_s.charAt(i) != new_s.charAt(j)) {
+                j = lps[j - 1];
+            }
+            if (new_s.charAt(i) == new_s.charAt(j)) {
+                j++;
+            }
+            lps[i] = j;
+        }
+        int max_palindrome_len = lps[lps.length - 1];
+        String to_add = rev_s.substring(0, s.length() - max_palindrome_len);
+        return to_add + s;
     }
 }
